8aaed02afea2eb84256d7a115b7e8f16bc4011ae
John Doe <juangnba@hotmail.com>
2023-03-24 15:31:34 +0100

    union-types

---
+++ union-types.js
@@ -0,0 +1,16 @@
+"use strict";
+(() => {
+    let userId;
+    userId = 12121;
+    userId = 'test';
+    function greeting(myText) {
+        if (typeof myText === 'string') {
+            console.log(`string ${myText.toLowerCase()}`);
+        }
+        else {
+            console.log(`number ${myText.toFixed(1)}`);
+        }
+    }
+    greeting('Test2');
+    greeting(12);
+});
---
+++ union-types.ts
@@ -0,0 +1,16 @@
+(() => {
+    let userId: string | number;
+    userId = 12121
+    userId = 'test'
+
+    function greeting(myText:string | number) {
+        if(typeof myText === 'string') {
+            console.log(`string ${myText.toLowerCase()}`)
+        } else {
+            console.log(`number ${myText.toFixed(1)}`)
+
+        }
+    }
+    greeting('Test2')
+    greeting(12) 
+})
